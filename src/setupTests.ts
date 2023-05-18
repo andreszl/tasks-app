fd5840a3af07d49fd76f517ae02fea039754b8de
andreszl <andreszl.devcode@gmail.com>
2023-05-18 07:39:03 -0500

    tasks

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,1 @@
 import '@testing-library/jest-dom';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'; // o 'enzyme-adapter-react-16' si estás utilizando React 16
-
-configure({ adapter: new Adapter() });
